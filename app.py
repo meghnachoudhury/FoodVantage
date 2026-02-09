f6b02d47db0d583af81e02351832d92c7b8c4a5c
Meghna Choudhury <44699511+meghnachoudhury@users.noreply.github.com>
2026-02-08 19:51:12 -0500

    Adjust focus-square top position from 50% to 70%

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ st.markdown(f"""
     
     .focus-square {{
         position: absolute;
-        top: 50%;
+        top: 70%;
         left: 50%;
         transform: translate(-50%, -50%);
         width: 200px;
