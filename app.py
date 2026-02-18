4f51b3d07a9e5dc8557a005fa709d351a35af258
Meghna Choudhury <44699511+meghnachoudhury@users.noreply.github.com>
2026-02-17 23:54:03 -0500

    Update app.py font size change

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ st.markdown(f"""
 
     .logo-text {{
         font-family: 'Source Serif 4', serif !important;
-        font-size: 2.0rem;
+        font-size: 1.95rem;
         text-align: left;
         font-weight: 700;
         letter-spacing: -0.6px;
